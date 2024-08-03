aa6b303e40dfab43754d7910b4dfac90ab04332d
Steve Jones <steve.jones763@gmail.com>
2024-08-03 16:55:36 +0100

    update hub IP to use hostname

--- sensor/setup.sh
+++ sensor/setup.sh
@@ -4,10 +4,10 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install python3-pip vim git -y
+sudo raspi-config nonint do_i2c 0
 git clone https://github.com/stevej763/home-hub.git
 cd home-hub/
 cd sensor/
 mkdir logs
-
-sudo raspi-config nonint do_i2c 0
+chmod 755 startup.sh
 sudo pip install pimoroni-bme280 --break-system-packages
